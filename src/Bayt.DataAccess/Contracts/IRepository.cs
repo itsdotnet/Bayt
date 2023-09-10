6e575d41f28cbc5e7a2f32f89d379311e39d99e3
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-10 22:34:48 +0500

    added Contracts

---
+++ src/Bayt.DataAccess/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using Bayt.Domain.Commons;
+
+namespace Bayt.DataAccess.Contracts;
+
+public interface IRepository<TEntity> where TEntity : Auditable
+{
+    Task CreateAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression = null,string[] includes = null);
+    IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null,string[] includes = null,
+        bool isTracking = true);
+}
---
+++ src/Bayt.DataAccess/Contracts/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+using Bayt.Domain.Entities.Addresses.Addresses;
+using Bayt.Domain.Entities.Addresses.Countries;
+using Bayt.Domain.Entities.Addresses.Districts;
+using Bayt.Domain.Entities.Addresses.Regions;
+using Bayt.Domain.Entities.Attachments;
+using Bayt.Domain.Entities.Companies;
+using Bayt.Domain.Entities.RealEstates;
+using Bayt.Domain.Entities.Roommates;
+using Bayt.Domain.Entities.Users;
+
+namespace Bayt.DataAccess.Contracts;
+
+public interface IUnitOfWork:IDisposable
+{
+    IRepository<User> UserRepository { get; }
+    IRepository<Attachment> AttachmentRepository { get; }
+    IRepository<Company> CompanyRepository { get; }
+    IRepository<Roommate> RoommateRepository { get; }
+    IRepository<RealEstate> RealEstateRepository { get; }
+    IRepository<Address> AddressRepository { get; }
+    IRepository<Country> CountryRepository { get; }
+    IRepository<Region> RegionRepository { get; }
+    IRepository<District> DistrictRepository { get; }
+
+    Task SaveAsync();
+}
