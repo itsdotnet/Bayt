5c32190953e15ce107b9cc5415164f47c52b7cc2
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-10 21:10:15 +0500

    added Attachment

---
+++ src/Bayt.Domain/Entities/Attachments/Attachment.cs
@@ -0,0 +1,9 @@
+using Bayt.Domain.Commons;
+
+namespace Bayt.Domain.Entities.Attachments;
+
+public class Attachment:Auditable
+{
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+}
