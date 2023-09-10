7eb5bc0445601c847a6a508bfb0d5b810caf6902
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-10 20:57:59 +0500

    added Roommate entity

--- src/Bayt.Domain/Entities/Roommates/Roommate.cs
+++ src/Bayt.Domain/Entities/Roommates/Roommate.cs
@@ -1,4 +1,6 @@
 using Bayt.Domain.Commons;
+using Bayt.Domain.Entities.Addresses.Addresses;
+using Bayt.Domain.Entities.Users;
 using Bayt.Domain.Enums;
 
 namespace Bayt.Domain.Entities.Roommates;
@@ -7,7 +9,13 @@ public class Roommate:Auditable
 {
     public string Description { get; set; }
     public Whom Whom { get; set; }
+    
     public decimal Rent { get; set; }
     public Currency Currency { get; set; }
-    
+
+    public Guid AddressId { get; set; }
+    public Address Address { get; set; }
+
+    public Guid UserId { get; set; }
+    public User User { get; set; }
 }
