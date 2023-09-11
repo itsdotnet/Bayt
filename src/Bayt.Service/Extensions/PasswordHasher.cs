ce13206b8f44603c7007612a2a65a1297aca7728
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-11 14:56:55 +0500

    PasswordHasher turned into clean code

--- src/Bayt.Service/Extensions/PasswordHasher.cs
+++ src/Bayt.Service/Extensions/PasswordHasher.cs
@@ -2,14 +2,9 @@
 
 public static class PasswordHasher
 {
-    public static string Hash(this string password)
-    {
-        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
-        return hashedPassword;
-    }
+    public static string Hash(this string password) 
+        => BCrypt.Net.BCrypt.HashPassword(password);
 
-    public static bool Verify(this string password, string hashedPassword)
-    {
-        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-    }
+    public static bool Verify(this string password, string hashedPassword) 
+        => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
 }
