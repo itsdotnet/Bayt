2f3b5a6b546e4c0251a62302614f444c9070ef38
iskandar kodirov <eiscocyber@gmail.com>
2023-09-11 12:16:52 +0500

    Media Helper Added to helpers

--- src/Bayt.Service/Helpers/CodeGenerator.cs
+++ src/Bayt.Service/Helpers/CodeGenerator.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Bayt.Service.Helpers;
 
-namespace Bayt.Service.Helpers
+public class CodeGenerator
 {
-    internal class CodeGenerator
+    public static int GenerateRandomNumber()
     {
+        Random random = new Random();
+        return random.Next(100000, 999999);
     }
 }
